e4302242136eac1ea20bd595a7c28da97572e1be
jo-wil <joey.wilson.a@gmail.com>
2016-11-14 12:23:11 -0800

    scraper working pretty well

--- wikicfp/scrape.py
+++ wikicfp/scrape.py
@@ -2,49 +2,65 @@
 
 import requests
 import re
+import json
 import time
+import bs4
 
-ROOT_URL = 'http://www.wikicfp.com/cfp/call?conference=computer%20science&page={}' # called with .format(<page number>)
-SINGLE_URL = 'http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid={}&copyownerid={}' # called with .format(<eventid>, <copyownerid>)
-
-OUT_FILE = None
+ROOT_URL = 'http://www.wikicfp.com/cfp/allcat'
+CATEGORY_URL = 'http://www.wikicfp.com/cfp/call?conference={}&page={}' # called with .format(<page number>)
+PAGE_URL = 'http://www.wikicfp.com/cfp/servlet/event.showcfp?eventid={}&copyownerid={}' # called with .format(<eventid>, <copyownerid>)
+DATABASE = []
 
 def scrape_page(html):
-   global OUT_FILE
-   f = OUT_FILE
+   global DATABASE
+   data = {}
+   m = re.search('<span property="v:description">([^<]+)</span>', html)
+   if m and m.group(1):
+      data['name'] = m.group(1).strip()
    m = re.search('Link: <a href="([^"]+)"', html)
-   f.write('Link: {}\n'.format(m.group(1)))
+   if m and m.group(1):
+      data['link'] = m.group(1).strip()
    m = re.search('<th>When</th>\s*<td align="center">([^<]+)</td>', html)
-   f.write('When: {}\n'.format(m.group(1).strip()))
+   if m and m.group(1):
+      data['when'] = m.group(1).strip()
    m = re.search('<th>Where</th>\s*<td align="center">([^<]+)</td>', html)
-   f.write('Where: {}\n\n'.format(m.group(1).strip()))
+   if m and m.group(1):
+      data['where'] = m.group(1).strip()
+   data['html'] = '' 
+   print(data['link'])
+   if data['link']:
+      r = requests.get(data['link'])
+      print(r.status_code)
+      if r.status_code == 200:
+         data['html'] = r.text  
+   DATABASE.append(data)
 
 def scrape_page_list(html):
-   m = re.findall('event\.showcfp\?eventid=([0-9]+)&amp;copyownerid=([0-9]+)', html)
-   for l in m:
-      print 'Working ...'
-      r = requests.get(SINGLE_URL.format(l[0], l[1]))
+   global PAGE_URL
+   for l in re.findall('event\.showcfp\?eventid=([0-9]+)&amp;copyownerid=([0-9]+)', html):
+      r = requests.get(PAGE_URL.format(l[0], l[1]))
       scrape_page(r.text)
 
+def scrape_category(category):
+   global CATEGORY_URL
+   print('Category: {}'.format(category))
+   for i in range(1, 2): # change to range(1,20) to get all of the pages
+      r = requests.get(CATEGORY_URL.format(category, i))
+      scrape_page_list(r.text)
+
 def scrape():
-   global ROOT_URL, SINGLE_URL
-   r = requests.get(ROOT_URL.format(1))
-   html = r.text
-   m = re.search('Total of ([0-9]+) CFPs in ([0-9]+) pages', html)
-   num_cfp = m.group(1)
-   num_pages = m.group(2)
-   print 'Number of CFPs: {}'.format(num_cfp)
-   print 'Number of pages: {}'.format(num_pages)
-   #for i in range(1, num+pages)
-   #   r = requests.get(ROOT_URL.format(i))
-   #   scrape_page_list(html)
-   scrape_page_list(html)
+   r = requests.get(ROOT_URL)
+   soup = bs4.BeautifulSoup(r.text, 'html.parser')
+   categories = map(lambda link: link.get_text(), soup.find_all('table')[3].find_all('a'))
+   for i in range(0, 1): # chage to for c in categories to get all the categories
+      scrape_category(categories[i])
 
 def main():
-   global OUT_FILE
-   OUT_FILE = open('output.txt', 'w')
+   global DATABASE
    scrape()
-   OUT_FILE.close()
+   f = open('output.json', 'w')
+   f.write(json.dumps(DATABASE, indent=3, separators=(',', ': ')))
+   f.close()
 
 if __name__ == '__main__':
    main()
