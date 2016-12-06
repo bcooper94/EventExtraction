a72c2a21fafae25fdaf0c7c5b93fefaad167f667
Brandon <bcooper94@github.com>
2016-12-05 18:28:41 -0800

    Forgot to shuffle corpus before splitting

--- wikicfp/splitCorpus.py
+++ wikicfp/splitCorpus.py
@@ -1,9 +1,12 @@
 import json
+import random
 
 
 def split_corpus(corpus_file: str, dev_ratio=0.8):
     with open(corpus_file) as file:
         corpus = json.load(file)
+        random.seed(35)
+        random.shuffle(corpus)
         dev_count = int(len(corpus) * dev_ratio)
         dev_set = corpus[:dev_count]
         test_set = corpus[dev_count:]
