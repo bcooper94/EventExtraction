0a716a0fb2e0f9256d8db27ea3e474028bf00340
Brandon <bcooper94@github.com>
2016-12-05 18:20:59 -0800

    Added script to split full json corpus into dev.json and test.json

---
+++ wikicfp/splitCorpus.py
@@ -0,0 +1,18 @@
+import json
+
+
+def split_corpus(corpus_file: str, dev_ratio=0.8):
+    with open(corpus_file) as file:
+        corpus = json.load(file)
+        dev_count = int(len(corpus) * dev_ratio)
+        dev_set = corpus[:dev_count]
+        test_set = corpus[dev_count:]
+
+        with open('dev.json', 'w') as dev:
+            json.dump(dev_set, dev, indent=3, separators=(',', ': '))
+        with open('test.json', 'w') as test:
+            json.dump(test_set, test, indent=3, separators=(',', ': '))
+
+
+if __name__ == '__main__':
+    split_corpus('output.json')
