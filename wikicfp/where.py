23fd62f09de64ad7b9b0c86fd08e08caca1c2c0a
jo-wil <joey.wilson.a@gmail.com>
2016-12-11 15:35:46 -0800

    where is almost done

--- wikicfp/where.py
+++ wikicfp/where.py
@@ -1,6 +1,7 @@
 # Author: Joey Wilson
 
 import json
+import random
 import nltk
 import bs4
 
@@ -10,17 +11,6 @@ from oauth2client.client import GoogleCredentials
 #debug
 def json_print(obj):
    print(json.dumps(obj, indent=3))
-
-#def extract_features(page):
-#   features = {}
-#
-#   return features
-#
-#   
-#def build_feature_list(data):
-#   feature_list = []
-#   for d in data:
-#      feature_list.append()
 
 #def n_gram(index, text, n):
 #   gram = []
@@ -32,13 +22,6 @@ def is_state_or_country(text, states, countries):
    if text.upper() in states or text.upper() in countries:
       return True
    return False
-#   if n_gram(index, text, 2) in states or n_gram(index, text, 2) in countries:
-#      return True
-#   if n_gram(index, text, 3) in states or n_gram(index, text, 3) in countries:
-#      return True
-#   if n_gram(index, text, 4) in states or n_gram(index, text, 4) in countries:
-#      return True
-#   return False 
 
 def is_closer(index, a, b):
    a_dist = index - a['text']['beginOffset']
@@ -103,36 +86,76 @@ def get_locations(page, states, countries):
             locations.append({'city': c, 'country_or_state': name})
 
    for l in locations:
-      _str = '{}, {}'.format(l['city'], l['country_or_state'])
+      ccs = '{}, {}'.format(l['city'], l['country_or_state'])
       index = 0
-      index = text.find(_str, index)
+      index = text.find(ccs, index)
+      l['index'] = []
       while index != -1:
+         l['index'].append(index)
          l['context'] = nltk.word_tokenize(text[index-50: index])
-         index = text.find(_str, index+1)
-
-   json_print(locations)
+         index = text.find(ccs, index+1)
 
    return locations
 
-#   locations = []
-#   for i in range(2, len(text)):
-#      if text[i-1] == ',' and is_state_country(i, text, states, countries):
-#         locations.append({
-#            'city': text[i-2],
-#            'country_or_state': text[i],
-#            'context': text[i-10: i]
-#         })
-#         print('{}, {}\nContext: {}'.format(text[i-2], text[i], text[i-10: i]))
-#
-#   return locations
- 
-#def run(training_data, test_data):
+def extract_features(location):
+   features = {}
 
-   #training_features = build_feature_list(training_data)
-   #test_features = build_feature_list(test_data)
+   #json_print(location)
+  
+   if 'index' in location.keys(): 
+      for index in location['index']:
+         for i in range(1,10):
+            if index < i*1000:
+               features['{} <= index < {}'.format(i-1, i)] = True
+   else:
+      features['No index'] = True
+
+   if 'context' in location.keys():
+      for w in location['context']:
+         features['contains({})'.format(w)] = True
+   else:
+      features['No context'] = True
+
+   return features
+
    
-   #classifier = nltk.NaiveBayesClassifier.train(training_features)
-   #print('Location: ', nltk.classify.accuracy(classifier, test_features))
+def build_feature_list(data, states, countries):
+   feature_list = []
+   
+   for d in data:
+      print(d['link'])
+      if d['where']:
+         ccs = d['where'].split(',')
+         if len(ccs) >= 2:
+            if len(ccs) == 3 and not is_state_or_country(ccs[1], states, countries):
+               print('ccs length 3 ', ccs)
+               ccs[1] = ccs[2] 
+            city = ccs[0].strip()
+            country_or_state = ccs[1].strip()
+            locations = get_locations(d['html'], states, countries)
+            print(city, country_or_state)
+            if len(locations) == 0:
+               print('No locations found')
+            else:
+               for l in locations:
+                  answer = False
+                  if l['city'] == city and l['country_or_state'] == country_or_state:
+                     answer = True
+                  feature_list.append((extract_features(l), answer))
+         else:
+            print('No city or country: {}'.format(ccs))
+      else:
+         print('No where found')
+
+   return feature_list
+
+def run(training_data, test_data, states, countries):
+
+   training_features = build_feature_list(training_data, states, countries)
+   test_features = build_feature_list(test_data, states, countries)
+   
+   classifier = nltk.NaiveBayesClassifier.train(training_features)
+   print('Location: ', nltk.classify.accuracy(classifier, test_features))
 
 def main():
    f = open('./output.json')
@@ -147,12 +170,13 @@ def main():
    countries = json.loads(f.read())
    f.close()
 
-   cfp = cfps[0]
+   data = cfps[0:60] 
 
-   print('Link: {} Where: {}'.format(cfp['link'], cfp['where']))
-
-   get_locations(cfp['html'], states, countries)
-
+   random.shuffle(data)   
+   
+   si = int(len(data)/4)
+   run(data[si:], data[0:si], states, countries)
+ 
 
 if __name__ == '__main__':
    main()
